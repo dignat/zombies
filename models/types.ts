5880a81c252a538d152af8f68e6e006eb2d5a7cd
Denise Ignatova <denis.ignatova@re-flow.co.uk>
2024-04-10 18:52:15 +0100

    new type m6

--- models/types.ts
+++ models/types.ts
@@ -1,3 +1,8 @@
 export const captureAndLog = (itemsToLog: unknown, message: string) => {
     console.info(itemsToLog, message)
 }
+
+export type ShoppingBasketError = {
+    type: string,
+    message: string
+}
